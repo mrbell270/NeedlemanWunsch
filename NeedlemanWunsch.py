b2b5a017dd72ee11f526bb49fe3e2daac8e29d21
mrbell270 <dead_mapos@mail.ru>
2015-09-19 13:44:20 +0300

    Initial Commit

---
+++ NeedlemanWunsch.py
@@ -0,0 +1,61 @@
+# coding=utf-8
+__author__ = 'Lenovo'
+
+
+def matrix_filling(seq, scores):
+    """
+    Filling matrix according to Needleman-Wunsch algorithm
+    :param seq: [2]string
+    :param scores: [3]int
+    :return: [][]int
+    """
+    f_matrix = [[0 for x in range(len(seq[1]) + 1)] for x in range(len(seq[0]) + 1)]  # Crating F matrix, init with 0s
+
+    for i in range(len(f_matrix)):                           #
+        if i:                                                #
+            f_matrix[i][0] = f_matrix[i - 1][0] + scores[2]  # Filling first row and column
+    for j in range(len(f_matrix[0])):                        #
+        if j:                                                #
+            f_matrix[0][j] = f_matrix[0][j - 1] + scores[2]  #
+
+    for i in range(len(f_matrix)):  # Filling all other cells
+        if not i:
+            continue
+        for j in range(len(f_matrix[i])):
+            if not j:
+                continue
+            delta = scores[int(seq[0][i - 1] == seq[1][j - 1])]
+            gap = scores[2]
+            f_matrix[i][j] = max(f_matrix[i - 1][j - 1] + delta,  #
+                                 f_matrix[i][j - 1] + gap,        # Choosing max of variants
+                                 f_matrix[i - 1][j] + gap)        #
+    return f_matrix
+
+
+def result_seq(f_matrix, seq, scores):
+    """
+    Finding trace in F matrix and getting resulting sequences
+    :param f_matrix: [][]int
+    :param seq: [2]string
+    :param scores: [3]int
+    :return: [2]string
+    """
+    res1 = ""
+    res2 = ""
+    i = len(seq[0])
+    j = len(seq[1])
+    while i > 0 or j > 0:
+        if i > 0 and j > 0 and f_matrix[i][j] == f_matrix[i - 1][j - 1] + scores[int(seq[0][i - 1] == seq[1][j - 1])]:
+            res1 = seq[0][i - 1] + res1
+            res2 = seq[1][j - 1] + res2
+            i -= 1
+            j -= 1
+        elif i > 0 and f_matrix[i][j] == f_matrix[i - 1][j] + scores[2]:
+            res1 = seq[0][i - 1] + res1
+            res2 = "-" + res2
+            i -= 1
+        else:
+            res1 = "-" + res1
+            res2 = seq[1][j - 1] + res2
+            j -= 1
+    return res1, res2
---
+++ main.py
@@ -0,0 +1,68 @@
+__author__ = 'Lenovo'
+import NeedlemanWunsch
+
+
+def print_matrix(m, seq):
+    """
+    Printing sequences and matrix
+    :param m: [][]int
+    :param seq: [2]string
+    :return:
+    """
+    print('{:10}'.format("")),
+    for char in seq[0]:
+        print('{:4}'.format(char)),
+    print
+    for i in range(len(m)):
+        if not i:
+            print('{:2}'.format("")),
+        else:
+            print('{:2}'.format(seq[1][i-1])),
+        for j in range(len(m[i])):
+            print('{:4}'.format(m[i][j])),
+        print
+
+
+def main():
+    seq = []
+    scores = [0, 0, 0]
+
+    filename = raw_input('File with sequences: ') # Input path to file, containing 2 sequences
+    if filename == '':
+        filename = r"C:\Users\Lenovo\PycharmProjects\NeedlemanWunsch\file.txt"
+    f = open(filename)
+    line = f.readline()
+    while line != '':
+        if line.startswith('>'):
+            line = f.readline()
+            tmp = ''
+            while not line.startswith('>') and line != '':
+                tmp += line.strip('\n')
+                line = f.readline()
+            seq.append(''.join(tmp.upper().split()))
+            continue
+        line = f.readline()
+    f.close()
+    print('Found sequences:\n{}\n{}\n'.format(seq[0], seq[1]))
+
+    scores[0] = -1   # mismatch
+    scores[1] = 1    # match
+    scores[2] = -1   # gap
+    print('Default scores:\nMismatch {}\nMatch {}\nGap {}\n'.format(scores[0], scores[1], scores[2]))
+    flag = raw_input('Do you want to change it? (y/n) ')
+    if flag == 'y':
+        scores[0] = int(raw_input('Mismatch score: '))   #
+        scores[1] = int(raw_input('Match score: '))      # Scores input
+        scores[2] = int(raw_input('Indel/gap score: '))  #
+    elif flag == 'n':
+        print('Working with default')
+
+    f_matrix = NeedlemanWunsch.matrix_filling(seq, scores)
+    # print_matrix(f_matrix, seq)
+
+    seq[0], seq[1] = NeedlemanWunsch.result_seq(f_matrix, seq, scores)
+    print('\nResult sequences:\n{}\n{}'.format(seq[0], seq[1]))
+    print('Score {}'.format(f_matrix.pop().pop()))
+
+if __name__ == '__main__':
+    main()
